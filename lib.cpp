b576b1785e6434d94e74c86da9a546e71b3af10f
gabrirusso <91951339+gabrirusso@users.noreply.github.com>
2021-12-01 11:31:36 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
 
+#include "lib.h"
+
+bool funzione (int a, int b){
+    bool esito;
+    if(b % a != 0 && b>a) {
+        funzione(a + 1, b);
+    } else if (b % a == 0 && b > a ){
+        return esito =  false ;
+    } else {
+        return esito = true ;
+    }
+}
